101d26b6f98085396242ccc51309db3a8716ec88
Roqyamohmad <roqyamohammadramdan@gmail.com>
2025-01-31 14:57:10 +0200

    adding sliding animation to splash view

---
+++ lib/features/Splash/presentation/views/widget/splash_view_body.dart
@@ -0,0 +1,44 @@
+import 'package:bookly_app/core/utils/assets.dart';
+import 'package:bookly_app/features/Splash/presentation/views/widget/sliding_text.dart';
+import 'package:flutter/material.dart';
+
+class SplashViewBody extends StatefulWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+  @override
+  void initState() {
+    super.initState();
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(minutes: 1));
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 10), end: Offset.zero)
+            .animate(animationController);
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.asset(AssetsData.Logo),
+        SlidingText(slidingAnimation: slidingAnimation),
+      ],
+    );
+  }
+}
